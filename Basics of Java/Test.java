39ac9988429dc20e4161b1e0a511099ffc90a49b
Om Khulbe <84437272+yashsharma2912@users.noreply.github.com>
2024-03-12 10:44:47 +0530

    Update Test.java

--- Basics of Java/Test.java
+++ Basics of Java/Test.java
@@ -1,5 +1,11 @@
 class Test{
     public static void main(String args[]){
+// premitive datatypes
+        int num = 654;
+        float dec = 3.444f;
+        boolean loggedIn = true;
+        long l = 4545454545454l;
+        short s = 454545;
            System.out.print("Hello World"); 
     }
 }
